32012eb5eb111a706f1d87b428e7ac2916b3519e
Valeri Y.Sergeev <valsy@mail.ru>
2020-07-26 10:25:06 +0300

    Dinamic web content

---
+++ 34/roles/react/files/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>otus-hw/34</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
